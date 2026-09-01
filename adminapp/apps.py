d768d868e93dab028d724d656879370506ccbcaf
C4lie <chandril.halder0@gmail.com>
2026-09-02 00:59:06 +0530

    feat(seed): add automatic production database seeder for City, Area, Hospitals, and UIP Vaccines

--- adminapp/apps.py
+++ adminapp/apps.py
@@ -1,4 +1,15 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def auto_seed_production_db(sender, **kwargs):
+    try:
+        from adminapp.models import City
+        if City.objects.count() == 0:
+            from django.core.management import call_command
+            call_command('seed_db')
+    except Exception:
+        pass
 
 
 class AdminappConfig(AppConfig):
@@ -6,3 +17,4 @@ class AdminappConfig(AppConfig):
 
     def ready(self):
         import kiddovax.signals  # noqa
+        post_migrate.connect(auto_seed_production_db, sender=self)
