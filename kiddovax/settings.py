83cd2adccfae960036db5776b8f0f454e297fca3
C4lie <chandril.halder0@gmail.com>
2026-09-02 00:46:05 +0530

    fix(deploy): update DB pooler settings with conn_max_age=0 and ssl_require=True

--- kiddovax/settings.py
+++ kiddovax/settings.py
@@ -110,8 +110,8 @@ if DATABASE_URL:
         DATABASES = {
             'default': dj_database_url.config(
                 default=DATABASE_URL,
-                conn_max_age=600,
-                conn_health_checks=True,
+                conn_max_age=0,
+                ssl_require=True,
             )
         }
     except Exception:
